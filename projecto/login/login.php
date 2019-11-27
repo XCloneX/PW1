86130b1c05c1e47de5b3811d75cbf1a89c5fadfc
sampaiofsky <sampaiofsky97@gmail.com>
2019-11-27 14:52:01 -0100

    crispier cleaner aimlockage

--- projecto/login/login.php
+++ projecto/login/login.php
@@ -6,11 +6,11 @@
     $file = fopen($filename,"r");
     $file2 = fopen($filename2,"a");
     $utilizadores=array();
-    $nick = $_POST['nick'] ;
-    $password = $_POST['passwd'] ;
-    $verdade = false ;
+    $nick = ($_POST['nick']);
+    $password = ($_POST['passwd']);
+    $verdade = false;
     $hora = date('H');
-    $radio = $_POST['checked'] ;
+    $radio = ($_POST['checked']);
     $horadeponto = date('d-m H:i');
     $assiduidades = array();
 
@@ -18,18 +18,16 @@
   // Verifica a existencia do utilizador enquanto percorre o ficheiro  e guarda o seu conteudo em um array
 while (($line = fgets($file,4096)) !== false ){
 
-
       $utilizador = explode(',' , $line) ;
       $utilizadores[$utilizador[0]] = $utilizador[1];
 
     if($utilizador[2] == $nick and $utilizador[3] == $password) {
         $verdade = true ;
-
         break;
     }
 }
   // Mensagem de erro caso o utilizador não exista ou as credenciais não corespondem
- if($verdade == false) {
+if($verdade == false) {
    echo "Este utilizador não se encontra registado/As credenciais nao correspondem" ;
 }
 
@@ -54,30 +52,34 @@ while (($line = fgets($f,4096)) !== false ){
 // Mostrar no ecrã uma mensagem com o nick do utilizador juntamente com um cumprimento consoante a hora do dia e se está a entrar ou a sair do trabalho
 if ($hora>5 && $hora<11) {
    if($radio == "in") {
-       echo "<br><br> Bom Dia $nick " . date('H:i') ."<br> Bom Trabalho"   ;}
-    else {
-     echo /*"Não pode sair a esta hora" */ "<br><br> Bom Dia $nick " . date('H:i') . "<br> Bom Descanso"; }
+       echo "<br><br> Bom Dia $nick " . date('H:i') ."<br> Bom Trabalho";
+   }
+   else {
+     echo /*"Não pode sair a esta hora" */ "<br><br> Bom Dia $nick " . date('H:i') . "<br> Bom Descanso";
+   }
 }
 if ($hora>11 && $hora<19) {
      if($radio == "in") {
-       echo "<br><br> Boa Tarde $nick " . date('H:i') . "<br> Bom Trabalho "  ;
+       echo "<br><br> Boa Tarde $nick " . date('H:i') . "<br> Bom Trabalho ";
       }
     if ($radio == "out"){
-      echo "<br><br> Boa Tarde $nick " . date('H:i') . "<br> Bom Descanso " ;
+      echo "<br><br> Boa Tarde $nick " . date('H:i') . "<br> Bom Descanso ";
     }
 }
 else {
   if($radio == "in") {
-    echo "<br><br> Boa Noite $nick " . date('H:i') . "<br> Bom Trabalho " . $radio  ;
+    echo "<br><br> Boa Noite $nick " . date('H:i') . "<br> Bom Trabalho ";
   }
     if ($radio == "out") {
-    echo "<br><br> Boa Noite $nick " . date('H:i') . "<br> Bom Descanso "    ;
+    echo "<br><br> Boa Noite $nick " . date('H:i') . "<br> Bom Descanso ";
     }
   }
+  //Escreve no ficheiro da assiduidade o utilizador, a hora de ponto e entrou ou saiu
+  fwrite($file2, ($_POST['nick']) .",". $horadeponto . "," . $radio . "," . "\n");
 }
 
 
-// Escreve no ficheiro da assiduidade o utilizador, a hora de ponto e entrou ou saiu
-fwrite($file2, $_POST['nick'] .",". $horadeponto . "," . $radio . "," . "\n");
+// Fecha os dois ficheiros
+
 fclose($file2);
 fclose($file);
